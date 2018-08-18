e2ad155554a8a16cf3e29d3b686bb3843443abb3
SilverHL <nelson.d.wang@gmail.com>
2018-08-18 19:45:55 +0800

    无

--- allocate.h
+++ allocate.h
@@ -7,31 +7,32 @@
 #include "malloctor.h"
 
 
+
 typedef __default_alloc_template<0, 0> default_alloc;
 typedef default_alloc alloc;
 
-template <class T, class Alloc>
-class simple_alloc
-{
-public:
-    static T *allocate(size_t n) { 
-        return 0 == n ? 0 : (T*)Alloc::allocate(n *sizeof(T)); 
-    }
-     
-    static T *allocate(void) {
-        return (T*)Alloc::allocate(sizeof(T));
-    }
+template <class T, class Alloc = alloc>
+    class simple_alloc
+    {
+    public:
+        static T *allocate(size_t n) { 
+            return 0 == n ? 0 : (T*)Alloc::allocate(n *sizeof(T)); 
+        }
 
-    static void deallocate(T *p, size_t n) {
-        if (n != 0) 
-            Alloc::deallocate(p, n *sizeof(T));
-    }
+        static T *allocate(void) {
+            return (T*)Alloc::allocate(sizeof(T));
+        }
 
-    static void deallocate(T *p) {
-        Alloc::deallocate(p, sizeof(T));
-    }
+        static void deallocate(T *p, size_t n) {
+            if (n != 0) 
+                Alloc::deallocate(p, n *sizeof(T));
+        }
 
-};
+        static void deallocate(T *p) {
+            Alloc::deallocate(p, sizeof(T));
+        }
+
+    };
 
 
 #endif
